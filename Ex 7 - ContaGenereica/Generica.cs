4768f0cc6a7e2e10c15635ea05412f5dfb7a53ac
IsadoraDias19 <isadora.d.souza@aluno.senai.br>
2024-11-26 09:25:25 -0300

    Adicionar arquivos de projeto.

---
+++ Ex 7 - ContaGenereica/Generica.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_7___ContaGenereica
+{
+    public abstract class Generica
+    {
+        public string Banco { get; set; }
+
+        public double Agencia { get; set; }
+
+        public string Conta { get; set; }
+
+        public double Saldo{ get; set; }
+
+        public abstract void Depositar();
+
+        public abstract void Sacar();
+    }
+
+    public class ContaCorrente1 : Generica 
+    { 
+        public double limite{ get; set; }
+        public override void Depositar()
+        {
+            Console.WriteLine("\nVocê tem que depositar 1.500");
+        }
+        public override void Sacar()
+        {
+            Console.WriteLine("Você tem que sacar 3.500");
+        }
+    }
+
+    public class ContaCorrente2 : Generica
+    {
+        public double limite { get; set; }
+        public override void Depositar()
+        {
+            Console.WriteLine("\nVocê tem que depositar 500");
+        }
+        public override void Sacar()
+        {
+            Console.WriteLine("Você tem que sacar 300");
+        }
+    }
+
+    public class ContaCorrente3 : Generica
+    {
+        public double limite { get; set; }
+        public override void Depositar()
+        {
+            Console.WriteLine("\nVocê tem que depositar 2.500");
+        }
+        public override void Sacar()
+        {
+            Console.WriteLine("Você tem que sacar 3.900");
+        }
+    }
+}
---
+++ Ex 7 - ContaGenereica/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Ex_7___ContaGenereica;
+
+ContaCorrente1 cont1 = new ContaCorrente1
+{
+    Banco = "Santander",
+    Agencia = 033,
+    Conta = "Marina da Silva",
+    Saldo = 4000,
+};
+ContaCorrente2 cont2 = new ContaCorrente2
+{
+    Banco = "Bradesco",
+    Agencia = 237,
+    Conta = "Cauã Leite Ferreira",
+    Saldo = 2000,
+};
+ContaCorrente3 cont3 = new ContaCorrente3
+{
+    Banco = "Nubank",
+    Agencia = 0001,
+    Conta = "Isadora Dias Cordeiro de Souza",
+    Saldo = 6000,
+};
+
+Console.WriteLine($"\nBanco:{cont1.Banco}, Agencia: {cont1.Agencia},a sua conta é {cont1.Conta},seu Saldo: {cont1.Saldo}");
+Console.WriteLine($"\nBanco:{cont2.Banco}, Agencia: {cont2.Agencia},a sua conta é {cont2.Conta},seu Saldo: {cont2.Saldo}");
+Console.WriteLine($"\nBanco:{cont3.Banco}, Agencia: {cont3.Agencia},a sua conta é {cont3.Conta},seu Saldo: {cont3.Saldo}");
+
+
+cont1.Depositar();
+cont1.Sacar();
+cont2.Depositar();
+cont2.Sacar();
+cont3.Depositar();
+cont3.Sacar();  
+
+Console.ReadKey();
